b43142c204a5ba43ee5bb7efcef3a21f98109883
gbh10c <grandcasa10@gmail.com>
2022-01-14 22:47:25 -0500

    TOP Object exercise 1/14

---
+++ more-array-exercises/11_getTheTitles/getTheTitles.js
@@ -0,0 +1,7 @@
+const getTheTitles = function(array) {
+  const titleArray = array.map(book => book.title);
+  return titleArray;
+};
+
+// Do not edit below this line
+module.exports = getTheTitles;
